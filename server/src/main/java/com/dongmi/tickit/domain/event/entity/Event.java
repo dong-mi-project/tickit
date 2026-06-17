4b5fd701c04dffbe3d8a2b5d37ca20da6e414418
진미강 <66945720+Luvdduk@users.noreply.github.com>
2026-06-17 21:29:18 +0900

    이벤트 도메인에 EventSchedule 클래스를 추가하고, EventController에 테스트 엔드포인트를 추가함. Event 엔티티의 구조를 개선하고, EventRepository를 JpaRepository를 확장하도록 수정함.

--- server/src/main/java/com/dongmi/tickit/domain/event/entity/Event.java
+++ server/src/main/java/com/dongmi/tickit/domain/event/entity/Event.java
@@ -1,12 +1,20 @@
 package com.dongmi.tickit.domain.event.entity;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
@@ -15,18 +23,46 @@ public class Event {
     
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-
+    @JdbcTypeCode(SqlTypes.BINARY)
+    private UUID id;
+    
+    @Column(nullable = false, updatable = false)
     private String userId;
+    @Column(nullable = false)
     private String title;
     @Lob
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private LocalDateTime deletedAt;
-    private String status;
-    private LocalDateTime startedAt;
-    private LocalDateTime endedAt;
-    private Integer roundNo;
+    @Column(nullable = false)
+    private LocalDateTime bookStartedAt;
+    @Column(nullable = false)
+    private LocalDateTime bookEndedAt;
+    @Column(nullable = false)
+    private String placeName;
+    @Column(nullable = false)
+    private String placeAddress;
+    @Column(nullable = false)
+    private Integer maxPerUser;
+
+    @Builder
+    public Event(String userId, String title, String description, LocalDateTime bookStartedAt, LocalDateTime bookEndedAt, String placeName, String placeAddress, Integer maxPerUser) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.bookStartedAt = bookStartedAt;
+        this.bookEndedAt = bookEndedAt;
+        this.placeName = placeName;
+        this.placeAddress = placeAddress;
+        this.maxPerUser = maxPerUser;
+    }
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
 
 }
--- server/src/main/java/com/dongmi/tickit/domain/event/repository/EventRepository.java
+++ server/src/main/java/com/dongmi/tickit/domain/event/repository/EventRepository.java
@@ -1,7 +1,11 @@
 package com.dongmi.tickit.domain.event.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends EventRepositoryCustom {
+import com.dongmi.tickit.domain.event.entity.Event;
+
+public interface EventRepository extends JpaRepository<Event, UUID>, EventRepositoryCustom {
 
 }
