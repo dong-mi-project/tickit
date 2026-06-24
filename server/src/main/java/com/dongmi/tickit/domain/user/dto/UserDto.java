c5c8ab5d74dd56147fc108efa6ca86b312d2905a
진미강 <66945720+Luvdduk@users.noreply.github.com>
2026-06-24 21:47:44 +0900

    코드정렬

--- server/src/main/java/com/dongmi/tickit/domain/user/dto/UserDto.java
+++ server/src/main/java/com/dongmi/tickit/domain/user/dto/UserDto.java
@@ -7,24 +7,24 @@ import jakarta.validation.constraints.NotBlank;
 public class UserDto {
     // 회원가입 요청 DTO
     public record SignUpRequest(
-            // 포커스 및 alert 주기 위한 설계
-            @NotBlank(message = "이름은 필수입니다.") String name,
-            @NotBlank(message = "이메일은 필수입니다.") @Email(message = "올바른 이메일 형식이 아닙니다.") String email,
-            @NotBlank(message = "비밀번호는 필수입니다.") String password,
-            @NotBlank(message = "전화번호는 필수입니다.") String phone
+        // 포커스 및 alert 주기 위한 설계
+        @NotBlank(message = "이름은 필수입니다.") String name,
+        @NotBlank(message = "이메일은 필수입니다.") @Email(message = "올바른 이메일 형식이 아닙니다.") String email,
+        @NotBlank(message = "비밀번호는 필수입니다.") String password,
+        @NotBlank(message = "전화번호는 필수입니다.") String phone
     ) {}
 
     // 로그인 요청 DTO
     public record LoginRequest(
-            @NotBlank(message = "이메일을 입력해주세요.") @Email String email,
-            @NotBlank(message = "비밀번호를 입력해주세요.") String password
+        @NotBlank(message = "이메일을 입력해주세요.") @Email String email,
+        @NotBlank(message = "비밀번호를 입력해주세요.") String password
     ) {}
 
     // 로그인 성공 응답 DTO
     public record LoginResponse(
-            String id,
-            String email,
-            String name,
-            String message
+        String id,
+        String email,
+        String name,
+        String message
     ) {}
 }
